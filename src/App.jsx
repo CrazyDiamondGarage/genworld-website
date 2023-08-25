1b9e96ecbb433a268b24142907f5c4ac0546bf86
Yuancheng Zhang <old.far.hot@gmail.com>
2023-08-26 04:32:15 +0800

    png => jpg

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <img id="bg-front" src="/img/bg/bg_01.png" alt="" />
+      <img id="bg-front" src="/img/bg/bg_01.jpg" alt="" />
       {/* <img id="bg-front" src="/img/bg/bg_01.jpg" alt="" /> */}
 
       <div className="cards">
-        <img className="card" id="card-5" src="/img/card/card_05.png" alt="" />
-        <img className="card" id="card-4" src="/img/card/card_04.png" alt="" />
-        <img className="card" id="card-3" src="/img/card/card_03.png" alt="" />
-        <img className="card" id="card-2" src="/img/card/card_02.png" alt="" />
-        <img className="card" id="card-1" src="/img/card/card_01.png" alt="" />
+        <img className="card" id="card-5" src="/img/card/card_05.jpg" alt="" />
+        <img className="card" id="card-4" src="/img/card/card_04.jpg" alt="" />
+        <img className="card" id="card-3" src="/img/card/card_03.jpg" alt="" />
+        <img className="card" id="card-2" src="/img/card/card_02.jpg" alt="" />
+        <img className="card" id="card-1" src="/img/card/card_01.jpg" alt="" />
       </div>
 
       <div className="titles">
