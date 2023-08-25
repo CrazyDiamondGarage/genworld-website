01e0cdd2cf0d741218f66107438a67007a640176
Yuancheng Zhang <old.far.hot@gmail.com>
2023-08-25 22:05:40 +0800

    update header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /**
  * @copyright Copyright 2023 CRAZY DIAMOND PTE LTD. All rights reserved.
  * @see Crazy Diamond: https://github.com/CrazyDiamondGarage
- * @see GenWorld: https://game.genworld.io/
+ * @see GenWorld: https://genworld.io/
  * @author endaye
  */
 
