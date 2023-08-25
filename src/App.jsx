53007f6823cbce2f0962bb2af7a4028b3e87857b
Yuancheng Zhang <old.far.hot@gmail.com>
2023-08-26 03:50:01 +0800

    update cards

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,13 @@ const App = () => {
       <img id="bg-front" src="/img/bg/bg_01.png" alt="" />
       {/* <img id="bg-front" src="/img/bg/bg_01.jpg" alt="" /> */}
 
+      <div className="cards">
+        <img className="card" id="card-5" src="/img/card/card_05.png" alt="" />
+        <img className="card" id="card-4" src="/img/card/card_04.png" alt="" />
+        <img className="card" id="card-3" src="/img/card/card_03.png" alt="" />
+        <img className="card" id="card-2" src="/img/card/card_02.png" alt="" />
+        <img className="card" id="card-1" src="/img/card/card_01.png" alt="" />
+      </div>
       <Footer />
     </>
   );
