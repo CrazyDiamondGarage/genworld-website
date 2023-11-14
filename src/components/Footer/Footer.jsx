323e2c2c064e7f812bf797ad43945076c23a3c24
Yuancheng Zhang <old.far.hot@gmail.com>
2023-11-14 13:29:55 +0800

    style(footer): 更新footer样式

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,35 +16,29 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-container">
-        <a href="https://genworld.io">
-          <img id="footer-logo" src="/img/logo/logo_01.png" alt="" draggable={false} />
-        </a>
-        <img className="footer-line" src="/img/icon/cutting_line.png" alt="" draggable={false} />
-        <a className="footer-social" href="https://twitter.com/GenWorld2023" target="_blank">
-          <h4>
-            <FaTwitter />
-          </h4>
-          <h5>Twitter</h5>
-        </a>
-        <a className="footer-social" href="https://discord.gg/Eu6ycbwnwY" target="_blank">
-          <h4>
-            <FaDiscord />
-          </h4>
-          <h5>Discord</h5>
-        </a>
-        <img className="footer-line" src="/img/icon/cutting_line.png" alt="" draggable={false} />
         <a className="footer-social" href="https://twitter.com/GenWorld2023" target="_blank">
           <h5>About Us</h5>
         </a>
+        <img className="footer-line" src="/img/icon/cutting_line.png" alt="" draggable={false} />
+
         <a className="footer-social" href="mailto:contact@genworld.io" target="_blank">
           <h5>Contact</h5>
         </a>
-        <a className="footer-social" href="https://genworld.notion.site/genworld/GenWorld-Terms-of-Service-c4a176057078458d9f3d3f25fa4f6d72" target="_blank">
-          <h5>Terms of Service</h5>
+        <img className="footer-line" src="/img/icon/cutting_line.png" alt="" draggable={false} />
+        <a
+          className="footer-social"
+          href="https://genworld.notion.site/genworld/GenWorld-Terms-of-Service-c4a176057078458d9f3d3f25fa4f6d72"
+          target="_blank">
+          <h5>Terms of Use</h5>
         </a>
-        <a className="footer-social" href="https://genworld.notion.site/GenWorld-Privacy-7c7274afce384cc29aabb95f941cfa1f" target="_blank">
+        <img className="footer-line" src="/img/icon/cutting_line.png" alt="" draggable={false} />
+        <a
+          className="footer-social"
+          href="https://genworld.notion.site/GenWorld-Privacy-7c7274afce384cc29aabb95f941cfa1f"
+          target="_blank">
           <h5>Privacy Policy</h5>
         </a>
+        <img className="footer-line" src="/img/icon/cutting_line.png" alt="" draggable={false} />
         <div id="footer-copyright">© 2023 CRAZY DIAMOND </div>
       </div>
     </div>
